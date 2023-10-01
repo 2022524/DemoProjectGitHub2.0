9e5a6e8234e5c38351f15dbdc6aa53e5133d0b5f
Tarquin <2022524@student.cct.ie>
2023-10-01 20:08:12 +0100

    Initial commit

---
+++ src/demoprojectgithub/DemoProjectGitHub.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demoprojectgithub;
+
+/**
+ *
+ * @author tarqu
+ */
+public class DemoProjectGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // A small program which will take an input
+        Scanner MyKB = new Scanner(System.in);
+
+        System.out.print("Please enter a number: ");
+        int userInput = MyKB.nextInt();
+        
+        
+    }
+    
+}
